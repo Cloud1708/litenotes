96a9fff3c36f5cf8e30517c453219712463f91fe
Cloud1708 <ch512291@gmail.com>
2026-03-22 23:12:02 +0800

    season 5

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -8,6 +8,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-alert-success>
+                {{ session('success') }}
+            </x-alert-success>
             <div class="flex">
                 <p class="opacity-70">
                     <strong>Created: </strong>{{ optional($note->created_at)->diffForHumans() ?? '-' }}
@@ -16,6 +19,12 @@
                 <p class="opacity-70 ml-8">
                     <strong>Updated at: </strong>{{ optional($note->updated_at)->diffForHumans() ?? '-' }}
                 </p>
+                <a href="{{ route('notes.edit', $note) }}" class="btn-link ml-auto">Edit Note</a>
+                <form action="{{ route('notes.destroy', $note) }}" method="POST">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note?')">Delete Note</button>
+                </form>
             </div>
 
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
